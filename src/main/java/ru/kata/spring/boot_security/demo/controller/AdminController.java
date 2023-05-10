e5a86c9f388afcb6098e50aa4b807978bdccb262
Ilya Kizilbashev <ikizil018@gmail.com>
2023-05-10 15:41:21 +0300

    10/05

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -0,0 +1,71 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.service.RoleService;
+import ru.kata.spring.boot_security.demo.service.UserService;
+import ru.kata.spring.boot_security.demo.model.User;
+
+
+@Controller
+public class AdminController {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private RoleService roleService;
+
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public AdminController(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping("/admin")
+    public String showAllUser(Model model, Authentication authentication) {
+        User user1 = (User)authentication.getPrincipal();
+        model.addAttribute("user1", user1);
+        model.addAttribute("allUser", userService.listUser());
+        model.addAttribute("user2", new User());
+        model.addAttribute("allRole", roleService.roleList());
+        return "index";
+    }
+
+    @GetMapping("/addUser")
+    public String addNewUser(Model model) {
+        model.addAttribute("user", new User());
+        model.addAttribute("allRole", roleService.roleList());
+        return "user-info";
+    }
+
+    @PostMapping("/saveUser")
+    public String saveUser(@ModelAttribute("user2") User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userService.saveUser(user);
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/updateUser")
+    public String updateUser(@RequestParam("userId") int id, Model model) {
+        model.addAttribute("user3", userService.getUser(id));
+        model.addAttribute("allRole", roleService.roleList());
+        return "user-update";
+    }
+
+    @PatchMapping("/upUser")
+    public String updateUser(@ModelAttribute("user") User user, @RequestParam("id") int id) {
+        userService.updateUser(id, user);
+        return "redirect:/admin";
+    }
+
+    @DeleteMapping("/deleteUser")
+    public String deleteUser(@RequestParam("id") int id, Model model) {
+        userService.deleteUser(id);
+        return "redirect:/admin";
+    }
+}
--- src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserController.java
@@ -1,41 +1,13 @@
 package ru.kata.spring.boot_security.demo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import ru.kata.spring.boot_security.demo.repositories.RoleRepository;
-import ru.kata.spring.boot_security.demo.service.RoleService;
-import ru.kata.spring.boot_security.demo.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
 import ru.kata.spring.boot_security.demo.model.User;
-
 
 @Controller
 public class UserController {
-    @Autowired
-    private UserService userService;
-
-    @Autowired
-    private RoleService roleService;
-
-    private PasswordEncoder passwordEncoder;
-
-    @Autowired
-    public UserController(PasswordEncoder passwordEncoder) {
-        this.passwordEncoder = passwordEncoder;
-    }
-
-    @GetMapping("/admin")
-    public String showAllUser(Model model, Authentication authentication) {
-        User user1 = (User)authentication.getPrincipal();
-        model.addAttribute("user1", user1);
-        model.addAttribute("allUser", userService.listUser());
-        model.addAttribute("user2", new User());
-        model.addAttribute("allRole", roleService.roleList());
-        return "index";
-    }
 
     @GetMapping("/user")
     public String showUser(Model model, Authentication authentication) {
@@ -43,39 +15,4 @@ public class UserController {
         model.addAttribute("user", user);
         return "user";
     }
-
-    @GetMapping("/addUser")
-    public String addNewUser(Model model) {
-        model.addAttribute("user", new User());
-        model.addAttribute("allRole", roleService.roleList());
-        return "user-info";
-    }
-    @PostMapping("/saveUser")
-    public String saveUser(@ModelAttribute("user2") User user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        userService.saveUser(user);
-        return "redirect:/admin";
-    }
-
-    @GetMapping("/updateUser")
-    public String updateUser(@RequestParam("userId") int id, Model model) {
-        model.addAttribute("user3", userService.getUser(id));
-        model.addAttribute("allRole", roleService.roleList());
-        return "user-update";
-    }
-
-
-    @PatchMapping("/upUser")
-    public String updateUser(@ModelAttribute("user") User user, @RequestParam("id") int id) {
-        userService.updateUser(id, user);
-        return "redirect:/admin";
-    }
-
-    @DeleteMapping("/deleteUser")
-    public String deleteUser(@RequestParam("id") int id, Model model) {
-        userService.deleteUser(id);
-        return "redirect:/admin";
-    }
-
-
 }
